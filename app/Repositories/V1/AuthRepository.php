a3d308ac1c57c8ee0074bc474e9512cc0c866a47
Ali Rıza Yücefaydalı <alirizayucefaydali@Alis-MacBook-Pro.local>
2025-01-30 16:37:53 +0100

    feat: Authentication functionalities with sanctum added

---
+++ app/Repositories/V1/AuthRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories\V1;
+
+use App\Models\V1\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthRepository
+{
+    public function register(array $data): array
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ['user' => $user, 'token' => $token];
+    }
+
+    public function login(array $data): array
+    {
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Invalid credentials.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ['user' => $user, 'token' => $token];
+    }
+
+    public function logout($user): void
+    {
+        $user->tokens()->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('/v1')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+        Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+
+        Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
+    });
+
+
+});
